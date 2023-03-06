f8cc8522bafc38029df0bc7d63274c432dc873d4
zakkiiiii <tsuzikt.tak@gmail.com>
2023-03-06 23:34:45 +0900

    create_model validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_063139) do
+ActiveRecord::Schema.define(version: 2023_03_06_080007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sample_articles", force: :cascade do |t|
     t.string "title"
